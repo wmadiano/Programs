2ea5299124883cd3749a0c08ede76fcad9b9f0c1
wmadiano <wmadiano@gmail.com>
2018-09-24 16:33:32 +0800

    change port
    change port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.port || 80;
+const port = process.env.port || 8080;
 const server = http.createServer(app);
 server.listen(port);
