f181b586d841fc7a9d28228b26757b18fc6a6db0
wmadiano <wmadiano@gmail.com>
2018-09-24 15:49:49 +0800

    change port
    to port 80

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.port || 3000;
+const port = process.env.port || 80;
 const server = http.createServer(app);
 server.listen(port);
